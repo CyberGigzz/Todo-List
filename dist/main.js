ba0aa0ced4d4e38b41d22fdbafb13e4805ba44d3
Giga_Dev148 <gigakatamadze0@gmail.com>
2023-07-24 18:24:32 +0400

    Building interface for a todo app

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("\"strict\"\n\n\n\n//# sourceURL=webpack://todo-list/./src/index.js?");
+eval("\"strict\";\n\nclass UI {\n  static initAddProjectButtons() {\n    const addProjectButton = document.getElementById(\"add-projects-button\");\n    const addProjectPopup = document.getElementById(\"add-project-popup\");\n    const addList = document.getElementById(\"add-list\");\n    const cancelProjectButton = document.getElementById(\"button-cancel-project-popup\");\n\n    // Add Project button\n    addProjectButton.addEventListener(\"click\", () => {\n      addList.classList.remove(\"active\");\n      addProjectPopup.classList.add(\"active\"); \n    });\n\n    // Cancel button\n    cancelProjectButton.addEventListener(\"click\", () => {\n      addList.classList.add(\"active\");\n      addProjectPopup.classList.remove(\"active\"); \n    });\n  }\n}\n\ndocument.addEventListener(\"DOMContentLoaded\", UI.initAddProjectButtons);\n\n\n//# sourceURL=webpack://todo-list/./src/index.js?");
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
-"strict"
+"strict";
 
+class UI {
+  static initAddProjectButtons() {
+    const addProjectButton = document.getElementById("add-projects-button");
+    const addProjectPopup = document.getElementById("add-project-popup");
+    const addList = document.getElementById("add-list");
+    const cancelProjectButton = document.getElementById("button-cancel-project-popup");
+
+    // Add Project button
+    addProjectButton.addEventListener("click", () => {
+      addList.classList.remove("active");
+      addProjectPopup.classList.add("active"); 
+    });
+
+    // Cancel button
+    cancelProjectButton.addEventListener("click", () => {
+      addList.classList.add("active");
+      addProjectPopup.classList.remove("active"); 
+    });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", UI.initAddProjectButtons);
