8c966eef088c5d994e7a45b481cee7f27acfae2c
Giga_Dev148 <gigakatamadze0@gmail.com>
2023-07-28 14:30:19 +0400

    creating lists as objects

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+"use strict";
+
+// Importing
+import { DynamicButton } from "./modules/UI";
+
 class UI {
   constructor() {
+    this.dynamicButtonsArray = [];
     this.initAddProjectButtons();
     this.initAddTasksButtons();
+    this.initAddListButton();
   }
 
   // Closing popups
@@ -57,6 +64,52 @@ class UI {
     cancelListButton.addEventListener("click", () => {
       addTasksPopup.classList.remove("active");
       addTask.classList.remove("active");
+    });
+  }
+
+  initAddListButton() {
+    const addListButton = document.getElementById("button-add-project-popup");
+    const addListInput = document.getElementById("input-add-project-popup");
+    const navElementsDiv = document.getElementById("nav-elements");
+
+    const closePopup = () => {
+      addListInput.value = ""; // Clear the input
+      addListInput.blur(); // Remove focus from the input
+      this.closeAllPopups(); // Close the popup
+    };
+
+    // const deleteButton = (button) => {
+    //   // Remove the button from the navElementsDiv
+    //   navElementsDiv.removeChild(button);
+    // };
+
+    addListButton.addEventListener("click", () => {
+      const newListName = addListInput.value.trim();
+
+      if (newListName !== "") {
+        const newButton = new DynamicButton(newListName);
+        this.dynamicButtonsArray.push(newButton);
+
+        console.log(this.dynamicButtonsArray);
+
+        navElementsDiv.appendChild(newButton.element);
+        addListInput.value = "";
+      }
+    });
+
+    // Handle the keydown event on the input
+    addListInput.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        addListButton.click(); // Trigger the "Add" button click event
+      } else if (event.key === "Escape") {
+        closePopup(); // Handle the "Escape" key to cancel
+      }
+    });
+
+    // Handle the click event on the "Cancel" button
+    const cancelButton = document.getElementById("button-cancel-project-popup");
+    cancelButton.addEventListener("click", () => {
+      closePopup();
     });
   }
 }
---
+++ src/modules/UI.js
@@ -0,0 +1,87 @@
+export class DynamicButton {
+  constructor(name) {
+    this.name = name;
+    this.active = false;
+    this.element = this.createButtonElement();
+
+    // Check if the element has the "active" class initially
+    if (this.element.classList.contains("active")) {
+      this.active = true;
+    }
+  }
+
+  createButtonElement() {
+    const button = document.createElement("button");
+    button.classList.add("button-list");
+
+    const iconSvg1 = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+    iconSvg1.classList.add("icon");
+    const use1 = document.createElementNS("http://www.w3.org/2000/svg", "use");
+    use1.setAttributeNS(
+      "http://www.w3.org/1999/xlink",
+      "xlink:href",
+      "./images/sprite.svg#list-check-solid"
+    );
+    iconSvg1.appendChild(use1);
+
+    const span = document.createElement("span");
+    span.textContent = this.name;
+
+    const iconSvg2 = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+    iconSvg2.classList.add("icon", "icon_x");
+    const use2 = document.createElementNS("http://www.w3.org/2000/svg", "use");
+    use2.setAttributeNS(
+      "http://www.w3.org/1999/xlink",
+      "xlink:href",
+      "./images/sprite.svg#x-solid"
+    );
+    iconSvg2.appendChild(use2);
+
+    iconSvg2.addEventListener("click", (event) => {
+      event.stopPropagation();
+      this.deleteButton();
+    });
+
+    button.appendChild(iconSvg1);
+    button.appendChild(span);
+    button.appendChild(iconSvg2);
+
+    button.addEventListener("click", () => {
+      this.handleButtonClick();
+    });
+
+    return button;
+  }
+
+  toggleActive() {
+    this.active = !this.active;
+  }
+
+  handleButtonClick() {
+    this.toggleActive();
+
+    const activeButton = document.querySelector(".button-list.active");
+    if (activeButton !== null) {
+      const activeButtonSpan = activeButton.querySelector("span");
+      activeButtonSpan.classList.remove("bold-text");
+      activeButton.classList.remove("active");
+    }
+
+    if (this.active) {
+      const span = this.element.querySelector("span");
+      span.classList.add("bold-text");
+      this.element.classList.add("active");
+    }
+  }
+
+  deleteButton() {
+    const navElementsDiv = document.getElementById("nav-elements");
+    navElementsDiv.removeChild(this.element);
+  }
+}
